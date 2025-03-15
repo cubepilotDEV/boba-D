8c26820282582fcac3f285c1f189f6703616411e
cubepilotDEV <157074568+cubepilotDEV@users.noreply.github.com>
2025-03-15 16:03:40 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('moreInfoBtn').addEventListener('click', function() {
-    var moreInfoText = document.getElementById('moreInfoText');
-    if (moreInfoText.style.display === "none") {
-        moreInfoText.style.display = "block";
-    } else {
-        moreInfoText.style.display = "none";
-    }
-});
